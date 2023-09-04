55b7b0a44cc9e6af6c2c41e45002cead46e912d4
Shijie Bian <sbian@phoebe-login>
2023-09-04 15:59:17 +0000

    removing checking of agent_name, since there are elseif-else bugs

--- worker.py
+++ worker.py
@@ -26,7 +26,5 @@ if __name__ == "__main__":
         learner_ip = socket.gethostbyname("mcar-learner-service")
         learner_address = (learner_ip, 4444)
         worker = AsnycWorker(learner_address=learner_address)
-    else:
-        print("Invalid Agent Name!")
 
     worker.work()
