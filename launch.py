2bda14863f5671d13f6b82b14de467c7921b3dc1
Shijie Bian <sbian@phoebe-login>
2023-10-02 21:25:25 +0000

    Revised launch.py for better UI

--- launch.py
+++ launch.py
@@ -1,16 +1,22 @@
 import subprocess
 
-# Define the RL agent ('mcar', 'walker', 'l2r')
-RL_agent = "walker"
+# Define the RL environment ('mcar', 'walker', 'l2r')
+RL_env = input("Select RL environment (mcar/walker/l2r): ").strip()
 
-# Define the training paradigm ('distribCollect', 'distribUpdate')
-training_paradigm = 'distribUpdate'
+# Define the training paradigm ('sequential', 'dCollect', 'dUpdate')
+training_paradigm = input("Select training paradigm (sequential/dCollect/dUpdate): ").strip()
+
+# Sanity check
+assert RL_env in ("mcar", "walker", "l2r")
+assert training_paradigm in ("sequential", "dCollect", "dUpdate")
 
 # Fetch the corresponding source file
-source_file = RL_agent + '-' + training_paradigm + '.yaml'
+source_file = RL_env + '-' + training_paradigm + '.yaml'
 
 # Print the info
-print(f"--- Launching [{RL_agent}] with training paradigm [{training_paradigm}]: {source_file} ---")
+print("---")
+print(f"--- Launching [{RL_env}] with training paradigm [{training_paradigm}]: kubernetes/{source_file} ---")
+print("---")
 
 # Launch Kubernetes source file
 try:
