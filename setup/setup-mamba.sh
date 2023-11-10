f170599515b63cca376a71ea6acc3b31079b08ab
Shijie Bian <sbian@phoebe-login>
2023-11-10 05:37:45 +0000

    creating unified setup-mamba script

--- setup/setup-mamba.sh
+++ setup/setup-mamba.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Install Anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
+chmod +x ./Anaconda3-2023.09-0-Linux-x86_64.sh
+./Anaconda3-2023.09-0-Linux-x86_64.sh -b
+source /root/anaconda3/bin/activate 
+
+# Install mamba
+conda create --name initialization python=3.8 -y
+conda activate initialization
+conda install -c conda-forge mamba -y
+
+# Create l2r environment using mamba
+mamba create --name l2r python=3.8 -y
+mamba init
+source /root/.bashrc 
+mamba activate l2r
+
 # Install necessary packages
 mamba install git -y
 mamba install -c conda-forge strictyaml -y
@@ -24,8 +41,15 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A4B469963BF863CC
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A4B469963BF863CC
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A4B469963BF863CC
 
-# Solve "version 'GLIBCXX_3.4.29' not found" error
+# Solve "version `GLIBCXX_3.4.29' not found" error
 # Ref: https://github.com/lhelontra/tensorflow-on-arm/issues/13#issuecomment-418202182
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
 sudo apt-get upgrade libstdc++6 -y
+
+# Prepare the repository
+cd /
+mkdir workspace
+cd workspace
+git clone https://github.com/BrandonBian/l2r-distributed.git
+cd l2r-distributed
