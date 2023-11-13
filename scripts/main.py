06e6921398c5295eeaf04f0f1ef6849e34d69e2d
Shijie Bian <sbian@phoebe-login>
2023-11-13 17:13:56 +0000

    sequential merge - using scripts.main and best_vae.pth from sequential

--- scripts/main.py
+++ scripts/main.py
@@ -1,28 +1,46 @@
-from hashlib import md5
-import subprocess
-#from l2r import build_env
-#from l2r import RacingEnv
 from src.config.yamlize import NameToSourcePath, create_configurable
-import sys
-import logging
-import gym
+import torch
+import argparse
 
 if __name__ == "__main__":
-    # Build environment
-    #env = build_env(controller_kwargs={"quiet": True})
-    env = gym.make("BipedalWalker-v3")
-    runner = create_configurable(
-        "config_files/example_sac/runner.yaml", NameToSourcePath.runner
+
+    # Argparse for environment selection and wandb config
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--env",
+        choices=["l2r", "mcar", "walker"],
+        help="Select the environment ('l2r', 'mcar', or 'walker')."
     )
 
-    with open(
-        f"{runner.agent.model_save_path}/{runner.exp_config['experiment_name']}/git_config",
-        "w+",
-    ) as f:
-        f.write(" ".join(sys.argv[1:3]))
-    # Race!
-    try:
-        runner.run(env, sys.argv[3])
-    except IndexError as e:
-        logging.warning(e)
-        runner.run(env, "")
+    parser.add_argument(
+        "--wandb_apikey",
+        type=str,
+        help="Enter your Weights-And-Bias API Key."
+    )
+
+    parser.add_argument(
+        "--exp_name",
+        type=str,
+        help="Enter your experiment name, to be recorded by Weights-And-Bias."
+    )
+
+    args = parser.parse_args()
+
+    # Initialize the runner and start run
+    print(f"Environment: {args.env} | Experiment: {args.exp_name}")
+
+    if args.env == "l2r":
+        runner = create_configurable(
+            "config_files/l2r_sac/runner.yaml", NameToSourcePath.runner)
+    elif args.env == "mcar":
+        runner = create_configurable(
+            "config_files/mcar_sac/runner.yaml", NameToSourcePath.runner)
+    elif args.env == "walker":
+        runner = create_configurable(
+            "config_files/walker_sac/runner.yaml", NameToSourcePath.runner)
+    else:
+        raise NotImplementedError
+
+    torch.autograd.set_detect_anomaly(True)
+    runner.run(args.wandb_apikey, args.exp_name)
