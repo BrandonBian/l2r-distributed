3ea1f9d6733c8d66d2891d379fe708023b7cf797
Shijie Bian <sbian@phoebe-login>
2023-12-04 18:56:31 +0000

    starting implementation o  SACVanilla

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -3,3 +3,4 @@ from src.agents.SACAgent import SACAgent
 from src.agents.PPOAgent import PPOAgent
 from src.agents.SACAgent_CleanRL import CleanSACAgent
 from src.agents.PETSAgent import PETSAgent
+from src.agents.SACVanilla import SACVanilla
